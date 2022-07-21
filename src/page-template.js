3fceea13865c3e03285715a90bab31abdbd83c12
Jonathan Thompson <jathompson399@gmail.com>
2022-07-20 21:47:39 -0500

    finished

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-// create the about section
+
 const generateAbout = aboutText => {
   if (!aboutText) {
     return '';
@@ -12,7 +12,6 @@ const generateAbout = aboutText => {
   `;
 };
 
-// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
@@ -38,6 +37,7 @@ const generateProjects = projectsArr => {
       ${projectsArr
         .filter(({ feature }) => !feature)
         .map(({ name, description, languages, link }) => {
+          console.log(languages);
           return `
           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="portfolio-item-title text-light">${name}</h3>
@@ -51,19 +51,20 @@ const generateProjects = projectsArr => {
         `;
         })
         .join('')}
+    
       </div>
     </section>
   `;
 };
 
-// export function to generate entire page
 module.exports = templateData => {
-  // destructure page data by section
+
   const { projects, about, ...header } = templateData;
 
   return `
   <!DOCTYPE html>
   <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -75,23 +76,25 @@ module.exports = templateData => {
   </head>
   
   <body>
+
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
         </nav>
       </div>
     </header>
+
     <main class="container my-5">
       ${generateAbout(about)}
       ${generateProjects(projects)}
     </main>
+
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
     </footer>
+
   </body>
   </html>
   `;
