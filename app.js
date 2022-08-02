494087926a2a6830e959cc839f387606ed508a9a
Jonathan Thompson <jathompson399@gmail.com>
2022-08-01 20:40:38 -0500

    finish lesson

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+// const printProfileData = profileDataArr => {
+
+//     // THESE ARE THE
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('=============');
+
+//     // SAME THING
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
 const promptUser = () => {
     return inquirer.prompt([ 
         {
@@ -129,33 +147,14 @@ const promptUser = () => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw err;
-  
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-// const printProfileData = profileDataArr => {
-
-//     // THESE ARE THE
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('=============');
-
-//     // SAME THING
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
